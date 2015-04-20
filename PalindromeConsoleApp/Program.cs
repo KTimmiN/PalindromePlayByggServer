8fe4c80c9ad7ac0646bc792c6dbc4d51362af0c5
nikqi <timmi_nikqi@hotmail.com>
2015-04-20 09:37:09 +0200

    Initialize new project

---
+++ PalindromeConsoleApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDPalindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PalindromeFinder p = new PalindromeFinder();
+            while(true)
+            {
+                Console.Write("Enter a word:");
+                var value = Console.ReadLine();
+                if (value == "q")
+                    break;
+                try
+                {
+                    if (p.IsPalindrome(value))
+                    {
+                        Console.WriteLine("Yey! The word is a palindrome");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nooeee, the word is not a palindrome");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
